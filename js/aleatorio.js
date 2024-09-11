b28ba732ddc5f3d0b4e4c215dfe173a49e4c837a
AntunesGk <goncalves.antunes@escola.pr.gov.br>
2024-09-11 15:25:55 -0300

    chiquinha poggers

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["AMOSTRADINHO", "KEN", "GABRIEL HENRIQUE RODRIGUES MENDES DE OLIVEIRA", "Marcelo", "Gustavo"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
